f418f83fe117895a49a8744ff51b9447d59bbd19
Anderson Banihirwe <axbanihirwe@ualr.edu>
2018-07-03 11:10:31 -0400

    add warning

--- destroy_all_images.sh
+++ destroy_all_images.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Warning: This will destroy all your images and containers. It will not be possible to restore them!
 # Delete all containers
 docker rm $(docker ps -a -q)
 # Delete all images
